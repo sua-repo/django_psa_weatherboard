5a2ee9cd1af9fc09a7104e8024df21b0156e7844
sua <dssua0714@naver.com>
2025-04-16 16:27:09 +0900

    미니게임 로또

--- game/urls.py
+++ game/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 app_name = "game"  # dev_3
 
-urlpatterns = [path("rsp/", views.rsp, name="rsp")]  # dev_3 : 가위바위보
+urlpatterns = [
+    path("rsp/", views.rsp, name="rsp"),  # dev_3 : 가위바위보
+    path("lotto/", views.lotto, name="lotto"),  # dev_4 : 로또
+]
--- game/views.py
+++ game/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 # dev_3
 def rsp(request):
     return render(request, "game/rsp.html")
+
+
+# dev_4
+def lotto(request):
+    return render(request, "game/lotto.html")
