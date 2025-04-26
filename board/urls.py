24144cb69abcd4f33a8ec340dadebbeef82c8a0d
sua <dssua0714@naver.com>
2025-04-26 18:22:11 +0900

    글 상세페이지 및 카테고리 필터링

--- board/urls.py
+++ board/urls.py
@@ -6,4 +6,7 @@ app_name = "board"
 urlpatterns = [
     path("", views.post_list, name="post_list"),  # dev_6 : 게시글 목록
     path("create/", views.post_create, name="post_create"),  # dev_8 : 게시글 작성
+    path(
+        "<int:post_id>/", views.post_detail, name="post_detail"
+    ),  # dev_9 : 게시글 작성
 ]
--- board/views.py
+++ board/views.py
@@ -7,8 +7,16 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # dev_6
+# dev_9 : 카테고리 필터링
 def post_list(request):
-    posts = Post.objects.order_by("-created_at")  # 최신순 정렬
+    category = request.GET.get('category')  # '일반' / '코디' / None
+    posts = Post.objects.all()
+
+    if category:  # 카테고리로 필터링
+        posts = posts.filter(category=category)
+
+    posts = posts.order_by("-created_at")
+
     paginator = Paginator(posts, 10)  # 한 페이지에 10개씩 보여주는 페이지네이터 생성
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(
@@ -18,6 +26,7 @@ def post_list(request):
     context = {
         "posts": page_obj,
         "today": date.today(),
+        "selected_category": category,
     }
     return render(request, "board/post_list.html", context)
 
@@ -29,7 +38,7 @@ def post_create(request):
         title = request.POST.get("title")
         content = request.POST.get("content")
         category = request.POST.get("category")
-        images = request.FILES.getlist('images')  # 여러 개 이미지 파일 가져오기
+        images = request.FILES.getlist("images")  # 여러 개 이미지 파일 가져오기
 
         # 먼저 글(Post) 저장
         post = Post.objects.create(
@@ -41,10 +50,17 @@ def post_create(request):
 
         # 그 Post에 연결해서 이미지들도 저장
         for img in images:
-            Image.objects.create(
-                post=post,
-                image=img
-            )
+            Image.objects.create(post=post, image=img)
         return redirect("board:post_list")  # 작성 후 글 목록으로 이동
 
     return render(request, "board/post_create.html")
+
+
+# dev_9
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    images = post.images.all()  # 해당 게시글에 연결된 이미지들
+
+    context = {"post": post, "images": images}
+
+    return render(request, "board/post_detail.html", context)
