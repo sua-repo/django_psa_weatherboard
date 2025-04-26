c31ef14883291d16b9dd0ce634fb82a0c19e25b1
sua <dssua0714@naver.com>
2025-04-27 02:03:59 +0900

    accounts 앱 생성 및 등록

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", include("main.urls")),  # dev_2 : 메인 페이지를 기본 경로로 연결
     path("game/", include("game.urls")),  # dev_3 : game 앱 연결
     path("board/", include("board.urls")),  # dev_6 : board 앱 연결
+    path('accounts/', include('accounts.urls')),    # dev_14 : accounts 앱 연결
 ]
 
 if settings.DEBUG:
